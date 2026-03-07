3bbfffab608a25ad9af87b50a9300989c9ac62ff
Gael Gomes <202408035268@alunos.unifbv.edu.br>
2026-03-07 00:48:01 -0300

    fix: corrige ligação do controller nos webhooks do Google - adicionado bind e removido metodo this

--- server/src/modules/webhooks/webhooks.routes.js
+++ server/src/modules/webhooks/webhooks.routes.js
@@ -4,8 +4,8 @@ const { verifyToken } = require("@/middlewares/authentication");
 
 const router = express.Router();
 
-router.get("/google/auth", verifyToken, webhooksController.googleAuth);
-router.get("/google/callback", webhooksController.googleCallback);
-router.post("/google/calendar", webhooksController.handleGoogleCalendarWebhook);
+router.get("/google/auth", verifyToken, webhooksController.googleAuth.bind(webhooksController));
+router.get("/google/callback", webhooksController.googleCallback.bind(webhooksController));
+router.post("/google/calendar", webhooksController.handleGoogleCalendarWebhook.bind(webhooksController));
 
 module.exports = router;
