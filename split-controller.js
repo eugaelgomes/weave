8d2eaa962c44dd38ba716471bc1335ada99b950f
Gael Gomes <hello@gaelgomes.dev>
2026-04-14 01:45:30 -0300

    fiz: comenta outras imagens que nao server e exclui arquivos orfas

--- split-controller.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('/home/gaelgomes/projetos/weave-notes/server/src/modules/users/users.controller.js', 'utf8');
-
-// A very simplistic approach: just read the whole file, export things to the appropriate files, but wait, the imports are needed in each.
-// Actually, it might be faster to just do it manually with regexes or provide a simple instruction.
-// Since time is short, I will generate the files one by one with a Node script that parses the AST or just regex, or use string manipulation.
