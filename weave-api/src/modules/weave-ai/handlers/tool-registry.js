178c18897b0e35690387b480b9e39622adfcb30c
Gael Gomes <hello@gaelgomes.dev>
2026-06-01 21:42:17 -0300

    fix .

--- weave-api/src/modules/weave-ai/handlers/tool-registry.js
+++ weave-api/src/modules/weave-ai/handlers/tool-registry.js
@@ -1,24 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 
-class ToolRegistry {{
-  constructor() {{
+class ToolRegistry {
+  constructor() {
     this.handlers = new Map();
     this._registerAll();
-  }}
+  }
 
-  _registerAll() {{
+  _registerAll() {
     const files = fs.readdirSync(__dirname).filter(f => f.endsWith('.handler.js'));
-    for (const file of files) {{
+    for (const file of files) {
       const handler = require(path.join(__dirname, file));
       const name = file.replace('.handler.js', '').replace(/-/g, '_');
       this.handlers.set(name, handler);
-    }}
-  }}
+    }
+  }
 
-  getHandler(name) {{
+  getHandler(name) {
     return this.handlers.get(name);
-  }}
-}}
+  }
+}
 
 module.exports = new ToolRegistry();
